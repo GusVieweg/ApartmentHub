e32d8738fccab534578fde7e04484e0f39c4ae3e
GusVieweg <awvieweg@gmail.com>
2016-05-11 18:20:39 -0400

    main screen consolidation

--- apartmentHub.py
+++ apartmentHub.py
@@ -1,47 +1,59 @@
 from kivy.app import App 
 from kivy.core.window import Window
-from kivy.uix.label import Label 
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, Rectangle
 from kivy.animation import Animation, AnimationTransition
+from kivy.uix.screenmanager import ScreenManager, Screen 
 
 
+#kivy.require("1.9.2")
 Window.fullscreen = 'auto'
 
 
-class RootWidget(FloatLayout):
+class MainScreen(Screen):
 
     def __init__(self, **kwargs):
         #make sure we aren't overriding any important functionality
-        super(RootWidget, self).__init__(**kwargs)
+        super(MainScreen, self).__init__(**kwargs)
+        f = FloatLayout()
 
-        #adding the widget
-        greeting_label = Label( text="Welcome home!", 
+        greeting_label = Label( text="Welcome home!",
                                 font_size=100,
                                 color=[1,1,1,0],
-                                pos=(self.x/2, (self.y/2 - 100)) )
+                                pos=(f.x/2, (f.y/2 - 100)) )
+        f.add_widget(greeting_label)
 
-        self.add_widget(greeting_label)
-        
-        anim =  Animation(color=[1.0, 1.0, 1.0, 1.0], 
-                          pos=(self.x/2, self.y/2),
-                          transition = AnimationTransition.in_out_quad) 
+        anim = Animation( color=[1.0, 1.0, 1.0, 1.0], 
+                           pos=(f.x/2, f.y/2),
+                           transition = AnimationTransition.in_out_quad ) 
         anim.start(greeting_label)
 
+        next_button = Button( text=">>",
+                              font_size=50,
+                              color=[1,1,1,1],
+                              size_hint=(0.1, 0.2),
+                              pos_hint={'x': 0.9, 'y': 0.4} )
+                              #size=(100, 100) )
 
-class mainScreenApp(App):
+        f.add_widget(next_button)
+
+        f.color=[(16.0/255.0), (23.0/255.0), (22.0/255.0), 1] #warm gray, converted to Kivy
+        #f.rect = Rectangle( size=f.size, pos=f.pos )
+        self.add_widget(f)
+
+class AnotherScreen(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+class MainscreenApp(App):
     def build(self):
-        self.root = root = RootWidget()
-        root.bind(pos=self.update_rect, size=self.update_rect)
+        self.root = root = MainScreen()
 
-        with root.canvas.before:
-            Color((16.0/255.0), (23.0/255.0), (22.0/255.0), 1) #warm gray, converted to Kivy
-            self.rect = Rectangle( size=root.size, pos=root.pos )
         return root
 
-    def update_rect(self, instance, value):
-        self.rect.pos = instance.pos
-        self.rect.size = instance.size
-
 if __name__ == '__main__':
-    mainScreenApp().run()
+    MainscreenApp().run()
